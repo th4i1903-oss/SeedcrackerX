629700a968fa661b2147e37069e5cc96c4b283f9
th4i1903-oss <th4i1903@gmail.com>
2026-07-24 20:46:54 +0700

    Create RedstoneOreFinder.java

---
+++ src/main/java/kaptainwutax/seedcrackerX/finder/decorator/ore/RedstoneOreFinder.java
@@ -0,0 +1,29 @@
+package kaptainwutax.seedcrackerX.finder.decorator.ore;
+
+import kaptainwutax.seedcrackerX.finder.Finder;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class RedstoneOreFinder extends OreFinder {
+
+    public RedstoneOreFinder(World world, ChunkPos chunkPos) {
+        super(world, chunkPos, List.of(
+            Blocks.REDSTONE_ORE,
+            Blocks.DEEPSLATE_REDSTONE_ORE
+        ));
+    }
+
+    @Override
+    public FinderType getType() {
+        return FinderType.ORE;
+    }
+
+    @Override
+    public List<BlockPos> findInChunk() {
+        return super.findInChunk();
+    }
+}
